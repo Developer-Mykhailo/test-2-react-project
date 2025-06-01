c4b8f6e22203dff6e6940d8db5bc040da91055c4
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-01 02:53:26 -0600

    Notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Container from "./components/Container/Container";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 import Options from "./components/Options/Options";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Description />
 
       <Options />
+      <Notification />
 
       <Feedback />
     </Container>
